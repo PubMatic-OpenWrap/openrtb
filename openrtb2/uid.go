246c6764c0631367f2097635328e55910de66e10
shalmali-patil <shalmali.patil@pubmatic.com>
2020-02-27 19:49:21 +0530

    adding eids related fields

---
+++ openrtb2/uid.go
@@ -0,0 +1,9 @@
+package openrtb2
+
+import "encoding/json"
+
+// Uid defines the contract for bidrequest.user.eids[i].uids[j]
+type Uid struct {
+	ID  string          `json:"id"`
+	Ext json.RawMessage `json:"ext,omitempty"`
+}
