5216ebe65c0ccbb4c86ef392869a67b526e8e317
shalmali-patil <shalmali.patil@pubmatic.com>
2020-02-28 18:48:22 +0530

    updating datatype of AType

--- uid.go
+++ uid.go
@@ -5,6 +5,6 @@ import "encoding/json"
 // Uid defines the contract for bidrequest.user.eids[i].uids[j]
 type Uid struct {
 	ID    string          `json:"id"`
-	AType string          `json:"atype,omitempty"`
+	AType int             `json:"atype,omitempty"`
 	Ext   json.RawMessage `json:"ext,omitempty"`
 }
