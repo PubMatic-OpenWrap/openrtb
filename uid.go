05c0ff6fdd563ae2ecbe062ad768b0ecfb2c813b
shalmali-patil <shalmali.patil@pubmatic.com>
2020-02-28 14:45:44 +0530

    adding atype field in Uid struct

--- uid.go
+++ uid.go
@@ -4,6 +4,7 @@ import "encoding/json"
 
 // Uid defines the contract for bidrequest.user.eids[i].uids[j]
 type Uid struct {
-	ID  string          `json:"id"`
-	Ext json.RawMessage `json:"ext,omitempty"`
+	ID    string          `json:"id"`
+	AType string          `json:"atype,omitempty"`
+	Ext   json.RawMessage `json:"ext,omitempty"`
 }
